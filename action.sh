452d01bd7984bd13786fef47f2f1f0005d82a013
OleksiyRudenko <oleksiy.rudenko@gmail.com>
2020-11-15 16:22:05 +0200

    fix(action): Add debug

--- action.sh
+++ action.sh
@@ -7,13 +7,13 @@ IF ${INPUT_GLOBAL}; then
   _GIT_GLOBAL_OPTION='--global'
 fi
 
-echo "INPUTS: global_flag=${_GIT_GLOBAL_OPTION} name=${INPUT_NAME} email=${INPUT_EMAIL} actor=${INPUT_ACTOR}"
+echo "INPUTS: global_flag=${_GIT_GLOBAL_OPTION} name=${INPUT_NAME} email=${INPUT_EMAIL} actor=${INPUT_ACTOR}" > gha.log
 
 INPUT_EMAIL="${INPUT_EMAIL:-'github-action@users.noreply.github.com'}"
 INPUT_NAME="${INPUT_NAME:-'GitHub Action'}"
 INPUT_ACTOR=${INPUT_ACTOR:-${GITHUB_ACTOR}}
 
-echo "VALUES: global_flag=${_GIT_GLOBAL_OPTION} name=${INPUT_NAME} email=${INPUT_EMAIL} actor=${INPUT_ACTOR}"
+echo "VALUES: global_flag=${_GIT_GLOBAL_OPTION} name=${INPUT_NAME} email=${INPUT_EMAIL} actor=${INPUT_ACTOR}"  >> gha.log
 
 git config $_GIT_GLOBAL_OPTION user.email "${INPUT_EMAIL}"
 git config $_GIT_GLOBAL_OPTION user.name "${INPUT_NAME}"
