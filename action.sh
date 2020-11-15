a5b38fa84987b7de36dbdda8659d2bc94fb1d8bf
OleksiyRudenko <oleksiy.rudenko@gmail.com>
2020-11-15 15:14:48 +0200

    fix(action options): Fix bug: option value part until first space is used only

--- action.sh
+++ action.sh
@@ -10,7 +10,7 @@ INPUT_EMAIL=${INPUT_EMAIL:-'github-action@users.noreply.github.com'}
 INPUT_NAME=${INPUT_NAME:-'GitHub Action'}
 INPUT_ACTOR=${INPUT_ACTOR:-${GITHUB_ACTOR}}
 
-git config $_GIT_GLOBAL_OPTION user.email ${INPUT_EMAIL}
-git config $_GIT_GLOBAL_OPTION user.name ${INPUT_NAME}
+git config $_GIT_GLOBAL_OPTION user.email "${INPUT_EMAIL}"
+git config $_GIT_GLOBAL_OPTION user.name "${INPUT_NAME}"
 git config $_GIT_GLOBAL_OPTION user.password ${INPUT_TOKEN}
 echo "GIT_USER=${INPUT_ACTOR}:${INPUT_TOKEN}" >> $GITHUB_ENV
